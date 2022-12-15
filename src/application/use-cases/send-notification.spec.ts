128d88994ad8c58c39cf812fc5c764341b25b69d
mariana <marilimabrito13@gmail.com>
2022-12-15 12:37:37 -0300

    criando casos de uso

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { SendNotification } from './send-notification';
+
+describe('Send notification',  () => {
+   it('should be able to send a notification', async () => {
+        const sendNotification = new SendNotification();
+    
+        const { notification } = await sendNotification.execute({
+          content: 'This is a notification',
+          category: 'social',
+          recipientId: 'example-recipient-id',
+        });
+        expect(notification).toBeTruthy();
+      });
+});
